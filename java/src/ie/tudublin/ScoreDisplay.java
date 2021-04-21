084a5a190f6176723a645d9f56541f31f94e3aa4
Dork_Soul <luke.hallinan@hotmail.com>
2021-04-21 19:11:17 +0100

    all notes displaying

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -8,9 +8,9 @@ public class ScoreDisplay extends PApplet
 {
 	ArrayList<Note> notes = new ArrayList<Note>();
 
-	//String score = "DEFGABcd";
+	String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
-	String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
+	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 
 	public void loadScore()
 	{
@@ -55,6 +55,82 @@ public class ScoreDisplay extends PApplet
 		}
 	}
 
+	private boolean isEnd = false;
+	private int whichTask = -1;
+	private int noteHeight;
+
+	private float border = 200;
+	private float noteSpace = 50;
+	float leftBorder = 60;
+	private char letter;
+
+	void displayScore()
+	{
+		
+		textSize(14);
+		textAlign(LEFT,CENTER);
+		
+		
+		textAlign(CENTER, CENTER);
+		strokeWeight(2);
+		stroke(0);
+		
+		//draw 5 lines across the screen evenly spaced
+		for(int i = 1 ; i <= 5 ; i ++)
+		{
+			float y = map(i, 1, 5, border, height - border);
+			line(leftBorder, y, width, y);
+		}
+
+		textAlign(LEFT, CENTER);
+
+		//iterate through array and draw notes
+		for(int i = 0 ; i < notes.size() ; i ++)
+		{
+			
+			fill(0);
+			noStroke();
+
+			letter = notes.get(i).getNote();
+
+			//set the note height
+			noteHeight = letter == 'D' ? 10: noteHeight;
+			noteHeight = letter == 'E' ? 9: noteHeight;
+			noteHeight = letter == 'F' ? 8: noteHeight;
+			noteHeight = letter == 'G' ? 7: noteHeight;
+			noteHeight = letter == 'A' ? 6: noteHeight;
+			noteHeight = letter == 'B' ? 5: noteHeight;
+			noteHeight = letter == 'c' ? 4: noteHeight;
+			noteHeight = letter == 'd' ? 3: noteHeight;
+			
+			//draw the note
+			float y1 = map(noteHeight, 1, 10, border, height - border + 10);
+			float  x1 = leftBorder + noteSpace * i;
+			circle(x1, y1, 20);
+
+			strokeWeight(4);
+			stroke(0);
+			line(x1+10, y1, x1+10, y1 - 50);
+
+			//draw tick if quaver
+			if(notes.get(i).getDuration() == 1){
+				line(x1+10, y1 - 50, x1+30, y1 - 40);
+			}
+
+			//draw note letters
+			// textAlign(CENTER, CENTER);
+			// text(str, x, y);
+			
+			
+
+			
+
+			
+		
+		}
+
+	}
+
 	public void settings()
 	{
 		size(1000, 500);
@@ -70,16 +146,17 @@ public class ScoreDisplay extends PApplet
 
 	public void setup() 
 	{
-		
+		colorMode(HSB);
 	}
 
 	public void draw()
 	{
 		background(255);
-		
+		displayScore();
 	}
 
 	void drawNotes()
 	{
+		
 	}
 }

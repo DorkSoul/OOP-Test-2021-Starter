632793c0d457b4fcba700f35e3676bc7fccba02a
Dork_Soul <luke.hallinan@hotmail.com>
2021-04-21 17:34:39 +0100

    scoreLoad and scorePrint working

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -8,19 +8,29 @@ public class ScoreDisplay extends PApplet
 {
 	ArrayList<Note> notes = new ArrayList<Note>();
 
-	String score = "DEFGABcd";
+	//String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
-	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
+	String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 
 	public void loadScore()
 	{
 
-		for(int i = 0 ; i <= score.length(); i++)
+		for(int i = 0 ; i <= (score.length()-1); i++)
 		{
+			
 			char c = score.charAt(i);
 			int number = 1;
 			char letter = c;
-			char check = score.charAt(i+1);
+			char check = c;
+
+			if(i >= (score.length()-1))
+			{
+				check = c;
+			}
+			else{
+				check = score.charAt(i+1);
+			}
+			
 
 			if(Character.isDigit(check)){
 				number = check - '0';
@@ -33,10 +43,16 @@ public class ScoreDisplay extends PApplet
 			Note note = new Note(letter, number);
 			notes.add(note);
 		}
-			
-		}
 	}
 	
+	public void printScore()
+	{
+		for(Note n:notes)
+		{
+			println(n);
+		}
+	}
+
 	public void settings()
 	{
 		size(1000, 500);
@@ -45,6 +61,9 @@ public class ScoreDisplay extends PApplet
 		char c = '7'; // c holds the character 7 (55)
 		int i = c - '0'; // i holds the number 7 (55 - 48) 
 		println(i);
+
+		loadScore();
+		printScore();
 	}
 
 	public void setup() 
